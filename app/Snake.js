42b745f0e3aed4b7b942883c438e7603146ca738
blouk <nicolas@blouk.com>
2016-05-05 16:02:53 +0200

    [LAB] Playing with cam

--- app/Snake.js
+++ app/Snake.js
@@ -12,27 +12,64 @@ System.register(['./modules/KeyEvent'], function(exports_1, context_1) {
         execute: function() {
             Snake = (function () {
                 function Snake() {
+                    this.snake_array = new Array();
+                    this.direction = KeyEvent_2.Key.right;
+                    this.fps = 20;
+                    this.size = 1;
                     this.scene = new THREE.Scene();
                     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+                    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+                    this.camera.position.z = 20 * this.size;
                     this.renderer = new THREE.WebGLRenderer({ alpha: true });
                     this.renderer.setSize(window.innerWidth, window.innerHeight);
                     document.body.appendChild(this.renderer.domElement);
-                    this.camera.position.z = 5;
                 }
                 Snake.prototype.create_snake = function () {
                     this.snake_length = 5;
-                    var geometry = new THREE.BoxGeometry(.5, .5, .5);
+                    var geometry = new THREE.BoxGeometry(this.size, this.size, this.size);
                     var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-                    for (var i = this.snake_length - 1; i >= 0; i++) {
+                    for (var i = this.snake_length; i >= 0; i--) {
+                        this.scene.remove(this.snake_array[i]);
+                    }
+                    this.snake_array = [];
+                    for (var i = this.snake_length; i >= 0; i--) {
                         var cube = new THREE.Mesh(geometry, material);
                         cube.position.set(i, 0, 0);
-                        this.scene.add(cube);
                         this.snake_array.push(cube);
+                        this.scene.add(cube);
                     }
                 };
-                Snake.prototype.render = function () {
+                Snake.prototype.move_snake = function () {
+                    this.camera.lookAt(this.snake_array[0].position);
+                    this.nx = this.snake_array[0].position.x;
+                    this.ny = this.snake_array[0].position.y;
+                    switch (this.direction) {
+                        case KeyEvent_2.Key.left:
+                            this.nx--;
+                            break;
+                        case KeyEvent_2.Key.up:
+                            this.ny++;
+                            break;
+                        case KeyEvent_2.Key.down:
+                            this.ny--;
+                            break;
+                        case KeyEvent_2.Key.right:
+                            this.nx++;
+                            break;
+                    }
+                    var tail = this.snake_array.pop(); //pops out the last cell
+                    tail.position.x = this.nx;
+                    tail.position.y = this.ny;
+                    this.snake_array.unshift(tail);
+                    for (var i = 0; i < this.snake_length; i++) {
+                    }
+                };
+                Snake.prototype.loop = function () {
                     var _this = this;
-                    requestAnimationFrame(function () { return _this.render(); });
+                    this.loop_timer = setInterval(function () { return _this.render(); }, 1000 / this.fps);
+                };
+                Snake.prototype.render = function () {
+                    this.move_snake();
                     this.renderer.render(this.scene, this.camera);
                 };
                 return Snake;
@@ -40,26 +77,15 @@ System.register(['./modules/KeyEvent'], function(exports_1, context_1) {
             ;
             StartGame = (function () {
                 function StartGame() {
+                    var speed = .01;
                     $(window).ready(function () {
                         var snake_game = new Snake();
-                        snake_game.render();
                         snake_game.create_snake();
+                        snake_game.render();
+                        snake_game.loop();
                         var keyevent = new KeyEvent_1.KeyEvent();
                         keyevent.KeyPress.on(function (e) {
-                            switch (e) {
-                                case KeyEvent_2.Key.up:
-                                    snake_game.camera.rotation.x += 0.1;
-                                    break;
-                                case KeyEvent_2.Key.down:
-                                    snake_game.camera.rotation.x -= 0.1;
-                                    break;
-                                case KeyEvent_2.Key.left:
-                                    snake_game.camera.rotation.y -= 0.1;
-                                    break;
-                                case KeyEvent_2.Key.right:
-                                    snake_game.camera.rotation.y += 0.1;
-                                    break;
-                            }
+                            snake_game.direction = e;
                         });
                     });
                 }
--- app/Snake.ts
+++ app/Snake.ts
@@ -9,39 +9,93 @@ class Snake {
     cube: THREE.Mesh;
     camera: THREE.PerspectiveCamera;
     renderer: THREE.WebGLRenderer;
-    snake_length:number;
-    snake_array:Array<THREE.Mesh>;
-
+    snake_length: number;
+    snake_array: Array<THREE.Mesh> = new Array<THREE.Mesh>();
+    direction: Key = Key.right
+    size: number;
+    nx: number;
+    ny: number;
+    fps: number = 20;
+    loop_timer: number;
 
     private timer;
     constructor() {
+        this.size = 1;
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+        this.camera.position.z = 20 * this.size;
         this.renderer = new THREE.WebGLRenderer({ alpha: true });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-
-
         document.body.appendChild(this.renderer.domElement);
-        this.camera.position.z = 5;
-
     }
 
     create_snake() {
         this.snake_length = 5;
-        var geometry = new THREE.BoxGeometry(.5, .5, .5);
+
+        var geometry = new THREE.BoxGeometry(this.size, this.size, this.size);
         var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        for(var i = this.snake_length-1;i>=0;i++){
-            var cube = new THREE.Mesh(geometry, material)
-            cube.position.set(i, 0, 0);
-            this.scene.add(cube);
-            this.snake_array.push(cube);
+        for (var i = this.snake_length; i >= 0; i--) {
+            this.scene.remove(this.snake_array[i]);
         }
+        this.snake_array = [];
+
+        for (var i = this.snake_length; i >= 0; i--) {
+            var cube: THREE.Mesh = new THREE.Mesh(geometry, material)
+            cube.position.set(i, 0, 0);
+            this.snake_array.push(cube);
+            this.scene.add(cube);
+        }
+
     }
 
 
-    render() {
-        requestAnimationFrame(() => this.render());
+    move_snake() {
+
+        this.camera.lookAt(this.snake_array[0].position);
+        this.nx = this.snake_array[0].position.x;
+        this.ny = this.snake_array[0].position.y;
+
+        switch (this.direction) {
+            case Key.left:
+                this.nx--;
+                break;
+
+            case Key.up:
+                this.ny++;
+                break;
+
+            case Key.down:
+                this.ny--;
+                break;
+
+            case Key.right:
+                this.nx++;
+                break;
+        }
+
+        var tail = this.snake_array.pop(); //pops out the last cell
+        tail.position.x = this.nx;
+        tail.position.y = this.ny;
+
+        this.snake_array.unshift(tail);
+
+
+        for (var i = 0; i < this.snake_length; i++) {
+
+            //this.snake_array[i].position.y = tail.position.y
+            //this.snake_array[i].position.x= tail.position.x
+        }
+    }
+
+    loop():void {
+
+        this.loop_timer = setInterval(() => this.render(), 1000/this.fps);
+    }
+
+    render():void {
+
+        this.move_snake();
         this.renderer.render(this.scene, this.camera);
     }
 };
@@ -51,36 +105,25 @@ class Snake {
 
 
 class StartGame {
+
     constructor() {
+        const speed: number = .01;
         $(window).ready(
             function() {
                 var snake_game = new Snake();
-                snake_game.render();
+
                 snake_game.create_snake();
+                snake_game.render();
+                snake_game.loop();
+
                 var keyevent = new KeyEvent();
                 keyevent.KeyPress.on((e: Key) => {
-                    switch (e) {
-                        case Key.up: snake_game.camera.rotation.x += 0.1;
-                            break;
 
-                        case Key.down: snake_game.camera.rotation.x -= 0.1;
-                            break;
+                    snake_game.direction = e;
 
-                        case Key.left:
-                            snake_game.camera.rotation.y -= 0.1;
-                            break;
-
-                        case Key.right:
-                            snake_game.camera.rotation.y += 0.1;
-                            break;
-                    }
-
-                }
-                    );
-            }
-            );
+                });
+            });
     }
-
 }
 
 
